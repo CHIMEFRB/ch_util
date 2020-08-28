7b2f6f60cfbd78d9ddff8ee2f458198052c66be1
anjakefala <anja.kefala@gmail.com>
2020-08-28 15:30:50 -0700

    feat(HKP): incorporate parallel_map into script
    - build up metric over time and then write to disk

--- scripts/hkp_to_prom.py
+++ scripts/hkp_to_prom.py
@@ -1,41 +1,64 @@
+import glob
+import os
+import re
+
+from datetime import datetime
 import pandas as pd
+from caput import mpiutil
 from ch_util.andata import HKPData
-import requests
 from mpi4py import MPI
 
 
-def worker(metric):
-    print('starting worker')
+def worker(h5file):
+    print(f'starting {h5file}')
 
     url = 'http://bao.chimenet.ca:3308/api/v1/import/prometheus'
-    f = HKPData.from_acq_h5("/home/anja/scratch/hkp_prom_20200801.h5", metrics=[metric])
-    df = f.select(metric)
+    metrics = HKPData.metrics(h5file)
+    prom_metric = ''
+    for metric in metrics:
+        f = HKPData.from_acq_h5(h5file, metrics=[metric])
+        df = f.select(metric)
 
 
-    for time, row in df.iterrows():
-        prom_metric = f"{metric}{{"
-        for col in df.columns:
-            # value is placed at the end of the metric
-            if col == 'value' or row[col] == "-":
-                continue
-            prom_metric += f"{col}=\"{row[col]}\", "
-        prom_metric = prom_metric[:-2]
-        prom_metric += f"}} "
-        # get the unix timestamp out of the pd.Timestamp object
-        prom_metric += f"{row['value']} {(time - pd.Timestamp('1970-01-01')) // pd.Timedelta('1ms')}"
-        params=f"upload_file={prom_metric}"
-        requests.post(url, params=params)
-    print(f'Finished {metric}')
+        for time, row in df.iterrows():
+            prom_metric += f"{metric}{{"
+            for col in df.columns:
+                # value is placed at the end of the metric
+                if col == 'value' or row[col] == "-":
+                    continue
+                prom_metric += f"{col}=\"{row[col]}\", "
+            prom_metric = prom_metric[:-2]
+            prom_metric += f"}} "
+            # get the unix timestamp out of the pd.Timestamp object
+            prom_metric += f"{row['value']} {(time - pd.Timestamp('1970-01-01')) // pd.Timedelta('1ms')}"
+            prom_metric += '\n'
+    print(f'finished {h5file}')
+    return prom_metric
+
+def find_files(start_time, end_time):
+    glob_str = os.path.join(
+            '/home/anja/projects/rpp-krs/chime/chime_online/', '*_chime_hkp', '*.h5'
+            )
+    list_of_files = sorted(glob.glob(glob_str))
+    final_list_of_files = []
+    for f in list_of_files:
+        folder_datetime = re.search("(\d*T\d*)Z.*", f).groups()[0]
+        folder_datetime = datetime.strptime(folder_datetime, "%Y%m%dT%H%M%S")
+        if folder_datetime >= start_time and folder_datetime <= end_time:
+            final_list_of_files.append(f)
+    return final_list_of_files
+
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-if rank == 0:
-    metrics = HKPData.metrics("/home/anja/scratch/hkp_prom_20200801.h5")
-else:
-    metrics = []
+list_of_files = find_files(datetime.strptime('20170701', '%Y%m%d'), datetime.strptime('20171201', '%Y%m%d'))
 
-metrics = comm.bcast(list(metrics), root=0)
 
-worker(metrics[rank])
+prom_metrics = mpiutil.parallel_map(worker, list_of_files, root=0)
+
+if mpiutil.rank0:
+    with open(f'/home/anja/scratch/prom-metric.txt', 'w') as print_metric:
+        for metric in prom_metrics:
+            print_metric.write(metric)
 
