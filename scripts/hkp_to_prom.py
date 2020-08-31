98d43d1092d67bab6442a6df1afdc1a03874485c
anjakefala <anja.kefala@gmail.com>
2020-08-31 11:14:38 -0700

    perf(HKP): misc performance improvements
    - instead of appending strings over time, collect pieces in list, and
    then join

--- scripts/hkp_to_prom.py
+++ scripts/hkp_to_prom.py
@@ -12,28 +12,30 @@ from mpi4py import MPI
 def worker(h5file):
     print(f'starting {h5file}')
 
+    with open(f'/home/anja/scratch/prom-{rank}.txt', 'a') as fp:
+        fp.write(f'{h5file}')
+
     url = 'http://bao.chimenet.ca:3308/api/v1/import/prometheus'
     metrics = HKPData.metrics(h5file)
-    prom_metric = ''
+    prom_metrics = []
+    f = HKPData.from_acq_h5(h5file, metrics=metrics)
     for metric in metrics:
-        f = HKPData.from_acq_h5(h5file, metrics=[metric])
         df = f.select(metric)
 
-
         for time, row in df.iterrows():
-            prom_metric += f"{metric}{{"
-            for col in df.columns:
-                # value is placed at the end of the metric
-                if col == 'value' or row[col] == "-":
-                    continue
-                prom_metric += f"{col}=\"{row[col]}\", "
-            prom_metric = prom_metric[:-2]
+            prom_metric = f"{metric}{{"
+            label_value_pairs = [f"{col}=\"{row[col]}\"" for col in df.columns if col != 'value' and row[col] != '-']
+            prom_metric += ", ".join(label_value_pairs)
             prom_metric += f"}} "
             # get the unix timestamp out of the pd.Timestamp object
             prom_metric += f"{row['value']} {(time - pd.Timestamp('1970-01-01')) // pd.Timedelta('1ms')}"
-            prom_metric += '\n'
+            prom_metrics.append(prom_metric)
+
+        with open(f'/home/anja/scratch/prom-{rank}.txt', 'a') as fp:
+            fp.write('\n'.join(prom_metrics))
+        prom_metrics = []
     print(f'finished {h5file}')
-    return prom_metric
+    return True
 
 def find_files(start_time, end_time):
     glob_str = os.path.join(
@@ -52,13 +54,7 @@ def find_files(start_time, end_time):
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-list_of_files = find_files(datetime.strptime('20170701', '%Y%m%d'), datetime.strptime('20171201', '%Y%m%d'))
+list_of_files = find_files(datetime.strptime('20170701', '%Y%m%d'), datetime.strptime('20170801', '%Y%m%d'))
 
 
-prom_metrics = mpiutil.parallel_map(worker, list_of_files, root=0)
-
-if mpiutil.rank0:
-    with open(f'/home/anja/scratch/prom-metric.txt', 'w') as print_metric:
-        for metric in prom_metrics:
-            print_metric.write(metric)
-
+success = mpiutil.parallel_map(worker, list_of_files, root=0)
